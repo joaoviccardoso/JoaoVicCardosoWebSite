d91cbc08b98f8b3aa1b093e2c722d7795217eaa4
seu nome de usuário <pealq63@gmail.com>
2026-01-13 22:54:44 -0300

    baixado o Router DOM para controlar rotas, e criado um componente simples para adiconar na pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import styled from "styled-components"
-import EstilosGlobais from "./Componentes/EstilosGlobais"
+import { Route, Routes } from "react-router-dom"
+import Nav from "./Componentes/Nav"
 
 function App() {
-  const FundoPagina = styled.div`
-    background: linear-gradient(#121212, #232323);
-    width: 100%;
-    height: 100vh;
-  `
 
   return (
-    <FundoPagina>
-      <EstilosGlobais/>
-    </FundoPagina>
+    <>
+      <header>
+        <Nav/>
+      </header>
+
+      <Routes>
+        
+      </Routes>
+    </>
   )
 }
 
