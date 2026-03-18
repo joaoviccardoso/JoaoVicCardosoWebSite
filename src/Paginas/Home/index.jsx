b2d89487d95af06764839345e2605c15b24d93ae
seu nome de usuário <pealq63@gmail.com>
2026-03-18 16:43:08 -0300

    foi adicionado as imagem e os texto da section Hero

--- src/Paginas/Home/index.jsx
+++ src/Paginas/Home/index.jsx
@@ -2,7 +2,21 @@ import CssHome from "./home.module.css"
 
 function Home(){
     return(
-        <h1 className={CssHome.h1}>Home</h1>
+        <section className={CssHome.sectionHero}>
+            <div className={CssHome.containerImagemHero}>
+                <span>
+                    <img src="src/assets/balaoOlaHome.png" alt="Caixa de mensagem Olá! Seja bem-vindo ao meu site." />
+                    <p>Olá! Seja bem-vindo ao meu site.</p>
+                </span>
+
+                <img src="src\assets\imagemBonecoJoaoHome.png" alt="Personagem Animado do João com um labtop" />
+            </div>
+
+            <div className={CssHome.containerTextoHero}>
+                <h1>Eu ajudo negócios a crescer <br/>na internet</h1>
+                <p>Desenvolvo sites que conectam sua empresa a mais clientes, de <br/> forma simples e profissional.</p>
+            </div>
+        </section>
     )
 }
 
