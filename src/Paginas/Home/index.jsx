7ba78853f83c05dd7ba13a172ae54fea3c213711
seu nome de usuário <pealq63@gmail.com>
2026-03-20 14:59:22 -0300

    foi criado a pasta sectionHero para organizar os arquivos da secao hero da pagina home

--- src/Paginas/Home/index.jsx
+++ src/Paginas/Home/index.jsx
@@ -1,35 +1,11 @@
 import CssHome from "./home.module.css"
 import BotaoCta from "../../Componentes/BotaoCta"
+import Hero from "./sections/sectionHero" 
 
 function Home(){
     return(
         <section className={CssHome.sectionContainerHero}>
-            <section className={CssHome.sectionHero}>
-                <div className={CssHome.containerImagemHero}>
-                    <span>
-                        <img src="src/assets/balaoOlaHome.png" alt="Caixa de mensagem Olá! Seja bem-vindo ao meu site." />
-                        <p>Olá! Seja bem-vindo ao meu site.</p>
-                    </span>
-
-                    <img src="src\assets\imagemBonecoJoaoHome.png" alt="Personagem Animado do João com um labtop" />
-                </div>
-
-                <div className={CssHome.containerTextoHero}>
-                    <h1>Eu ajudo negócios a crescer <br/>na internet</h1>
-                    <p>Desenvolvo sites que conectam sua empresa a mais clientes, de <br/> forma simples e profissional.</p>
-                </div>
-
-                <div className={CssHome.containerBotaoCta}>
-                    <BotaoCta
-                        child="Comece seu projeto hoje"
-                        to=""
-                    />
-                    <BotaoCta
-                        child="Comece seu projeto hoje"
-                        to=""
-                    />
-                </div>
-            </section>
+            <Hero/>
 
             <section className={CssHome.secao}>
                 <div className={CssHome.containerPresencaDigital}>
---
+++ src/Paginas/Home/sections/sectionHero/index.jsx
@@ -0,0 +1,35 @@
+import CssHero from "./hero.module.css"
+import BotaoCta from "../../../../Componentes/BotaoCta"
+
+function Hero(){
+    return(
+        <section className={CssHero.sectionHero}>
+                <div className={CssHero.containerImagemHero}>
+                    <span>
+                        <img src="src/assets/balaoOlaHome.png" alt="Caixa de mensagem Olá! Seja bem-vindo ao meu site." />
+                        <p>Olá! Seja bem-vindo ao meu site.</p>
+                    </span>
+
+                    <img src="src\assets\imagemBonecoJoaoHome.png" alt="Personagem Animado do João com um labtop" />
+                </div>
+
+                <div className={CssHero.containerTextoHero}>
+                    <h1>Eu ajudo negócios a crescer <br/>na internet</h1>
+                    <p>Desenvolvo sites que conectam sua empresa a mais clientes, de <br/> forma simples e profissional.</p>
+                </div>
+
+                <div className={CssHero.containerBotaoCta}>
+                    <BotaoCta
+                        child="Comece seu projeto hoje"
+                        to=""
+                    />
+                    <BotaoCta
+                        child="Comece seu projeto hoje"
+                        to=""
+                    />
+                </div>
+        </section>
+    )
+}
+
+export default Hero
