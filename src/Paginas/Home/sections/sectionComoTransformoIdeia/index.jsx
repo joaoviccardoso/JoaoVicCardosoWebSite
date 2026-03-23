e517d778a8f477ce144c223fa981bdd64b11a0c9
seu nome de usuário <pealq63@gmail.com>
2026-03-23 16:47:25 -0300

    Criado o design da tela de desktop utilizando display grid, foi colocado o estilo dos texto e na imagem

--- src/Paginas/Home/sections/sectionComoTransformoIdeia/index.jsx
+++ src/Paginas/Home/sections/sectionComoTransformoIdeia/index.jsx
@@ -4,35 +4,35 @@ import CssComoTranformoIdeia from "./comoTranformoIdeia.module.css"
 
 function ComoTranformoIdeia(){
     return(
-        <section className={CssComoTranformoIdeia.secaoComoTransformoIdeias} style={{backgroundImage: `url(${logoDev})`,backgroundSize: '500px' ,backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
-            <div className={CssComoTranformoIdeia}>
-                <h3>Como Transformo Ideias em Resultados Digitais</h3>
-                <p>Meu trabalho vai além da programação. É um processo estratégico que combina três pilares:</p>
+        <section className={CssComoTranformoIdeia.secaoComoTransformoIdeias} style={{backgroundImage: `url(${logoDev})`,backgroundSize: '90%' ,backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
+            <div className={CssComoTranformoIdeia.containerTituloDaSecao}>
+                <h3>Como Transformo Ideias em <br/>Resultados Digitais</h3>
+                <p>Meu trabalho vai além da programação. É um processo estratégico <br/>que combina três pilares:</p>
             </div>
 
-            <ul>
-                <li>
+            <ul className={CssComoTranformoIdeia.ulLista}>
+                <li className={CssComoTranformoIdeia.Lista1}>
                     <ContainerTexto
                         titulo="Desenvolvimento Web"
                         texto="Crio landing pages impactantes e sistemas web robustos, combinando tecnologia de ponta com designs responsivos que funcionam perfeitamente em todos os dispositivos. Sua presença online com performance excepcional."                
                     />
                 </li>
 
-                <li>
+                <li className={CssComoTranformoIdeia.Lista2}>
                     <ContainerTexto
                         titulo="Desenvolvimento Web"
                         texto="Crio landing pages impactantes e sistemas web robustos, combinando tecnologia de ponta com designs responsivos que funcionam perfeitamente em todos os dispositivos. Sua presença online com performance excepcional."                
                     />
                 </li>
 
-                <li>
+                <li className={CssComoTranformoIdeia.Lista3}>
                     <ContainerTexto
                         titulo="Desenvolvimento Web"
                         texto="Crio landing pages impactantes e sistemas web robustos, combinando tecnologia de ponta com designs responsivos que funcionam perfeitamente em todos os dispositivos. Sua presença online com performance excepcional."                
                     />
                 </li>
 
-                <li>
+                <li className={CssComoTranformoIdeia.Lista4}>
                     <ContainerTexto
                         titulo="Desenvolvimento Web"
                         texto="Crio landing pages impactantes e sistemas web robustos, combinando tecnologia de ponta com designs responsivos que funcionam perfeitamente em todos os dispositivos. Sua presença online com performance excepcional."                
