5ea4910261d5b16f2be409c1c66f0a32cff70f01
seu nome de usuário <pealq63@gmail.com>
2026-03-27 15:09:05 -0300

    criado a animação de quanto a pessoa entra na pagina tambem foi arrumado tamanho da fonte por tamanho de telas

--- src/Paginas/Home/sections/sectionHero/index.jsx
+++ src/Paginas/Home/sections/sectionHero/index.jsx
@@ -1,26 +1,72 @@
+import { useLayoutEffect, useRef } from "react";
+import { gsap } from "gsap";
+import SplitText from "gsap/src/SplitText";
 import CssHero from "./hero.module.css"
 import BotaoCta from "../../../../Componentes/BotaoCta"
 import balaoMensagemHola from "../../../../assets/balaoOlaHome.png"
 import imagemBonecoJoaoHome from "../../../../assets/imagemBonecoJoaoHome.png"
 
 function Hero(){
+    const titleRef = useRef(null)
+
+    useLayoutEffect(() =>{
+        let split;
+        const tl = gsap.timeline();         
+        gsap.registerPlugin(SplitText)
+        document.fonts.ready.then(() => {
+            split = new SplitText(titleRef.current, {
+            types: "words",
+        });
+
+        tl.from(split.words, {
+                duration: 1.3,
+                opacity: 0,
+                y: 30,
+                stagger: 0.08,
+                ease: "power2.out",
+                delay: 0.5
+            })
+            .from("#containerBalaoOla", {
+                x: 50,
+                opacity:0,
+                rotateY: -90
+           }, "-=0.8")
+           .from("#personagemAnimadoHero", {
+                x: 50,
+                opacity:0,
+                rotateY: -90
+           }, "-=0.8")
+            .from("#subtituloHero", {
+                opacity: 0,
+                y: 20,
+            }, "-=0.8")
+            .from("#containerBotaoHero", {
+                opacity: 0,
+                scale: 0.9,
+            }, "-=0.3")
+           ;
+        });
+
+        return
+    }, [])
+
     return(
         <section className={CssHero.sectionHero}>
                 <div className={CssHero.containerImagemHero}>
-                    <span>
+                    <span id="containerBalaoOla">
                         <img src={balaoMensagemHola} alt="Caixa de mensagem Olá! Seja bem-vindo ao meu site."/>
                         <p>Olá! Seja bem-vindo ao meu site.</p>
                     </span>
 
-                    <img src={imagemBonecoJoaoHome} alt="Personagem Animado do João com um labtop" />
+                    <img id="personagemAnimadoHero" src={imagemBonecoJoaoHome} alt="Personagem Animado do João com um labtop" />
                 </div>
 
                 <div className={CssHero.containerTextoHero}>
-                    <h1>Eu ajudo negócios a crescer <br/>na internet</h1>
-                    <p>Desenvolvo sites que conectam sua empresa a mais clientes, de <br/> forma simples e profissional.</p>
+                    <h1 ref={titleRef}>Eu ajudo negócios a crescer <br/>na internet</h1>
+                    <p id="subtituloHero">Desenvolvo sites que conectam sua empresa a mais clientes, de <br/> forma simples e profissional.</p>
                 </div>
 
-                <div className={CssHero.containerBotaoCta}>
+                <div className={CssHero.containerBotaoCta} id="containerBotaoHero">
                     <BotaoCta
                         child="Comece seu projeto hoje"
                         to=""
