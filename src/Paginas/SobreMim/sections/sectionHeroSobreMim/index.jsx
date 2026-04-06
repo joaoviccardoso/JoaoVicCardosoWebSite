a3a63241f4a9ca6e56b96f898249d5755dae79da
seu nome de usuário <pealq63@gmail.com>
2026-04-06 16:44:52 -0300

    foi criado a secao hero da pagina sobre mim, foi desenvolvido para desktop e celulares

--- src/Paginas/SobreMim/sections/sectionHeroSobreMim/index.jsx
+++ src/Paginas/SobreMim/sections/sectionHeroSobreMim/index.jsx
@@ -1,9 +1,21 @@
 import CssSobreMim from "./heroSobreMim.module.css"
+import BotaoCta from "../../../../Componentes/BotaoCta"
+import imgPersonagemHero from "../../../../assets/personagemPaginaSobreMim.png"
 
 function SecaoHeroSobreMim(){
     return(
-        <section>
+        <section className={CssSobreMim.secaoHeroSobreMim}>
+            <div className={CssSobreMim.containerTextoSobreMimHero}>
+                <h1>Transformo ideias em produtos digitais.</h1>
+                <p>Projetos bem planejados, design estratégico e desenvolvimento focado em performance e crescimento.</p>
+                <div>
+                    <BotaoCta
+                        child="Comece seu projeto hoje"
+                    />
+                </div>
+            </div>
 
+            <img src={imgPersonagemHero} className={CssSobreMim.imgSobreMim} alt="um quadrado com as borda verde com um boneco no centro" />
         </section>
     )
 }
