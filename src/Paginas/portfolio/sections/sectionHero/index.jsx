2d0def8ca3ef652d34ba25e101cc3979ae639232
seu nome de usuário <pealq63@gmail.com>
2026-04-13 15:25:54 -0300

    criado os arquivos da pagina produtos

---
+++ src/Paginas/portfolio/sections/sectionHero/index.jsx
@@ -0,0 +1,9 @@
+import CssHero from "./heroProdutos.module.css"
+
+function HeroProdutos(){
+    return (
+        <section></section>
+    )
+}
+
+export default HeroProdutos
---
+++ src/Paginas/portfolio/sections/sectionProdutos/index.jsx
@@ -0,0 +1,15 @@
+import CssProduto from "./produtos.module.css"
+
+function Produtos(){
+    return(
+        <section>
+            <div>
+
+            </div>
+
+            <ul></ul>
+        </section>
+    )
+}
+
+export default Produtos
