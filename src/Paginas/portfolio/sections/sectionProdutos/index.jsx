210db9734b4e94087bbda78f40c7a90c7de94919
seu nome de usuário <pealq63@gmail.com>
2026-04-13 16:35:52 -0300

    foi criado uma api fake para os produtos meus, e para os clientes

--- src/Paginas/portfolio/sections/sectionProdutos/index.jsx
+++ src/Paginas/portfolio/sections/sectionProdutos/index.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react"
 import CssProduto from "./produtos.module.css"
 import LinkParaNavegacao from "../../../../Componentes/Links/link"
 import menuCelular from "../../../../assets/menuCelular.svg"
 
 function Produtos(){
+    const [produto, setProduto] = useState([]);
+
+   useEffect(() => {
+    async function buscarDados() {
+        try {
+        const res = await fetch("http://localhost:3000/produtos");
+        const data = await res.json();
+        setProduto(data);
+        } catch (erro) {
+        console.error(erro);
+        }
+    }
+
+    buscarDados();
+    }, []);
+
+    console.log(produto)
     return(
         <section className={CssProduto.secaoProdutos}>
             <div className={CssProduto.filtroProdutos}>
