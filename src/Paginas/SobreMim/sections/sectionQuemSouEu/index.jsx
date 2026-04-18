b887a6d3d2d893d3323932fccc70c8ba3a8875a5
joaoviccardoso <pealq63@gmail.com>
2026-04-18 10:43:10 -0300

    feito a responsividade da pagina sobre mim para telas de 1000px ate 1500px

--- src/Paginas/SobreMim/sections/sectionQuemSouEu/index.jsx
+++ src/Paginas/SobreMim/sections/sectionQuemSouEu/index.jsx
@@ -5,7 +5,7 @@ import imgJoaoDev from "../../../../assets/fotoDoJoaoDesenvolvedorDaPagina.png"
 function SecaoQueSouEu(){
     return(
         <section className={CssQuemSouEu.secaoQuemSouEu}>
-            <img className={CssQuemSouEu} src={imgJoaoDev} alt="Foto do desenvolvedor da pagina" />
+            <img className={CssQuemSouEu.imagemQuemSou} src={imgJoaoDev} alt="Foto do desenvolvedor da pagina" />
 
             <div className={CssQuemSouEu.divQuemSouEuTexto}>
                 <h3>Desenvolvedor focado em transformar ideias em soluções digitais</h3>
