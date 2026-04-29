96a4a52baebc42903b3dc2ea2312562923ea4e7f
seu nome de usuário <pealq63@gmail.com>
2026-04-29 16:05:37 -0300

    criado os conteudo das paginas de acoes

--- src/Paginas/DashboardAdm/SubDashboardClienteAdm/index.jsx
+++ src/Paginas/DashboardAdm/SubDashboardClienteAdm/index.jsx
@@ -1,17 +1,29 @@
 import { useEffect, useState } from "react"
 import CssSubDashAdm from "./subDashAdm.module.css"
 import { pegarUser } from "../../../Utils/pegarUser"
+import BotaoDash from "../../../Componentes/BotaoDashBoard"
 
 function SubDashAdmCliente(){
     const [userAdm, setUserAdm] = useState({})
     
-        useEffect(() => {
-            setUserAdm(pegarUser())
-        }, [])
+    useEffect(() => {
+        setUserAdm(pegarUser())
+    }, [])
+
     return(
-        <section className={CssSubDashAdm}>
+        <section className={CssSubDashAdm.sectionFuncoesClientes}>
             <h1>Bem vindo, {`${userAdm.nomeCompleto}`}</h1>
             <p>Esse dashboard mostra tudo relacionado a Clientes.</p>
+            <div>
+                <BotaoDash
+                    child="Consultar Clientes"
+                    to="/admin/DashBoardCliente/ConsultarCliente"
+                />
+                <BotaoDash
+                    child="Cadastrar Clientes"
+                    to="/admin/DashBoardCliente/CadastrarCliente"
+                />
+            </div>
         </section>
     )
 }
--- src/Paginas/DashboardAdm/SubDashboardProdutosAdm/index.jsx
+++ src/Paginas/DashboardAdm/SubDashboardProdutosAdm/index.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react"
+import { pegarUser } from "../../../Utils/pegarUser"
 import CssSubDashAdmProdutos from "./subDashAdmProdutos.module.css"
+import BotaoDash from "../../../Componentes/BotaoDashBoard"
 
 function SubDashAdmProdutos(){
+    const [userAdm, setUserAdm] = useState({})
+        
+    useEffect(() => {
+        setUserAdm(pegarUser())
+    }, [])
+
     return(
-        <section>
-            <h1>Sub produtos</h1>
+        <section className={CssSubDashAdmProdutos.sectionProdutosAdm}>
+            <h1>Bem vindo, {`${userAdm.nomeCompleto}`}</h1>
+            <p>Esse dashboard mostra tudo relacionado a projetos.</p>
+            <div>
+                <BotaoDash
+                    child="Produto Clientes"
+                    to="/admin/DashBoardProduto/ProdutoClientes"
+                />
+                <BotaoDash
+                    child="Produto MP"
+                    to="/admin/DashBoardProduto/ProdutoMP"
+                />
+                <BotaoDash
+                    child="Cadastrar MP"
+                    to="/admin/DashBoardProduto/CadastrarMP"
+                />
+            </div>
         </section>
     )
 }
