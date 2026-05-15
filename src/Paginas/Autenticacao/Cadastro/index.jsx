8be67fb81d8bda86759a0dc1ec372e0024706c55
seu nome de usuário <pealq63@gmail.com>
2026-05-15 18:04:52 -0300

    url 2.0

--- src/Paginas/Autenticacao/Cadastro/index.jsx
+++ src/Paginas/Autenticacao/Cadastro/index.jsx
@@ -42,7 +42,7 @@ function TelaCadastro(){
             return;
         }
 
-        const response = await fetch("https://teal-goose-884697.hostingersite.com/auth/register", {
+        const response = await fetch("https://lightslategray-deer-405894.hostingersite.com/auth/register", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
