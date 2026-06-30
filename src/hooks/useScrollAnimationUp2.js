c0e0e908b56ba566cc3983af810773cce83f8c12
seu nome de usuário <pealq63@gmail.com>
2026-06-30 13:29:10 -0300

    minhas alterações locais

--- src/hooks/useScrollAnimationUp2.js
+++ src/hooks/useScrollAnimationUp2.js
@@ -14,7 +14,6 @@ export function useScrollAnimationUp2(el, ids) {
             const tl = gsap.timeline({
                 scrollTrigger: {
                     trigger: elements[0] ?? el.current,
-                    markers:true,
                     scrub: 1,
                     start: "-350% 100%",
                     end: "-300% 60%",
