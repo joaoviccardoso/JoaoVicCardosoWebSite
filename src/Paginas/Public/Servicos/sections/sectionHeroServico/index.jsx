4db78fd5ccb0034c83baec2f6d16eccdcbdd616d
joaoViccardoso <pealq63@gmail.com>
2026-07-14 11:49:54 -0300

    melhorado o hero da pagina servico

--- src/Paginas/Public/Servicos/sections/sectionHeroServico/index.jsx
+++ src/Paginas/Public/Servicos/sections/sectionHeroServico/index.jsx
@@ -16,72 +16,7 @@ function HeroServico(){
     const divHero = useRef(null);
     const el = useRef(null);
 
-    useLayoutEffectHeroPadrao(el,titleRef,textRef,imgHeroServico,divBotao, divHero)
-
-    /*useLayoutEffect(() => {
-    gsap.registerPlugin(SplitText, ScrollTrigger);
-
-    const ctx = gsap.context(() => {
-        let split;
-
-        document.fonts.ready.then(() => {
-            split = new SplitText(titleRef.current, {
-                types: "words",
-            });
-
-            const intro = gsap.timeline({
-                onComplete: () => {
-                    // 👇 ScrollTrigger só nasce aqui
-                    gsap.timeline({
-                        scrollTrigger: {
-                            trigger: el.current,
-                            scrub: 2,
-                            start: "top 90%",
-                            end: "bottom 50%",
-                        }
-                    })
-                    .to(divHero.current, {
-                        y: -150,
-                        opacity: .7,
-                        scale: 0.95,
-                        filter: "blur(5px)"
-                    },1)
-                    .to(imgHeroServico.current, {
-                        y: -150,
-                        opacity: .7,
-                        scale: 0.95,
-                        filter: "blur(5px)"
-                    }, 1)
-                }
-            });
-
-            intro
-                .from(split.words, {
-                    duration: 1.3,
-                    opacity: 0,
-                    y: 30,
-                    stagger: 0.08,
-                    ease: "power2.out",
-                    delay: 0.5
-                })
-                .from(textRef.current, {
-                    y: 30,
-                    opacity: 0,
-                }, "-=0.8")
-                .from(divBotao.current, {
-                    y: 30,
-                    opacity: 0,
-                }, "-=0.8")
-                .from(imgHeroServico.current, {
-                    opacity: 0,
-                    x: 40,
-                }, "-=0.3")
-            });
-
-        }, el);
-
-        return () => ctx.revert();
-    }, []);*/
+    useLayoutEffectHeroPadrao(el,titleRef,textRef,imgHeroServico,divBotao, el)
 
     return(
         <section className={CssHeroServico.secaoHeroServico} ref={el}>
