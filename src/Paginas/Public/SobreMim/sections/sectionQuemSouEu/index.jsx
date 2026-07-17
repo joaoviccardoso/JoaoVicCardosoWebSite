24897ec478bc97946261af0d5057b407b35c9ca0
seu nome de usuário <pealq63@gmail.com>
2026-07-16 22:47:51 -0300

    feito o layout da secao quem sou eu

--- src/Paginas/Public/SobreMim/sections/sectionQuemSouEu/index.jsx
+++ src/Paginas/Public/SobreMim/sections/sectionQuemSouEu/index.jsx
@@ -4,13 +4,18 @@ import { ScrollTrigger } from "gsap/ScrollTrigger"
 import CssQuemSouEu from "./quemSouEu.module.css"
 import BotaoCta from "../../../../../Componentes/Buttons/BotaoCta"
 import imgJoaoDev from "../../../../../assets/fotoDoJoaoDesenvolvedorDaPagina.png"
-
+//import { useFadeInScroll } from "../../../../../hooks/useScrollAnimationFadeIn"
 gsap.registerPlugin(ScrollTrigger)
 
 function SecaoQueSouEu(){
     const wrapperRef = useRef(null)
     const trackRef = useRef(null)
-
+    const contHabili = useRef(null)
+    const contMenta = useRef(null)
+    const item1 = useRef(null)
+    const item2 = useRef(null)
+    const item3 = useRef(null)
+    
     useLayoutEffect(() => {
         const ctx = gsap.context(() => {
             ScrollTrigger.matchMedia({
@@ -44,7 +49,7 @@ function SecaoQueSouEu(){
         return () => ctx.revert()
     }, [])
 
-    
+    //useFadeInScroll(contHabili)
 
     return(
         <section>
@@ -72,18 +77,18 @@ function SecaoQueSouEu(){
                     <div className={CssQuemSouEu.containerSkills}>
 
                         {/* Habilidades */}
-                        <div className={CssQuemSouEu.boxSkills}>
+                        <div className={CssQuemSouEu.boxSkills} ref={contHabili}>
                             <div className={CssQuemSouEu.containerTitulosSkills}>
-                                <h5 className={CssQuemSouEu.tituloSkills1}>
+                                <h5 className={CssQuemSouEu.tituloSkills1} ref={item1}>
                                     Habilidades
                                 </h5>
 
-                                <h5 className={CssQuemSouEu.tituloSkills2}>
+                                <h5 className={CssQuemSouEu.tituloSkills2} ref={item2}>
                                     Técnico
                                 </h5>
                             </div>
                             
-                            <div className={CssQuemSouEu.linhaSkills}>
+                            <div className={CssQuemSouEu.linhaSkills} ref={item3}>
                                 <h3>Performance</h3>
                                 <h3>UI Moderna</h3>
                                 <h3>Escalável</h3>
@@ -91,7 +96,7 @@ function SecaoQueSouEu(){
                         </div>
 
                         {/* Mentalidade */}
-                        <div className={CssQuemSouEu.boxSkills}>
+                        <div className={CssQuemSouEu.boxSkills} ref={contMenta}>
                             <div className={CssQuemSouEu.containerTitulosMentalidade}>
                                 <h5 className={CssQuemSouEu.tituloMentalidade1}>
                                     Minha
